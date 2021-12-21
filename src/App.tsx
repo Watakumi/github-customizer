6095f48f398dce874ceec9d6cc6fa4ac806dd9b9
石渡拓海 <t-ishiwata@sight-visit.com>
2021-12-22 02:28:30 +0900

    フォントとカラーを適用する

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Helmet } from 'react-helmet';
 
 function App() {
-  return <h1 className="text-5xl font-bold underline">Hello world!</h1>;
+  return (
+    <>
+      <Helmet>
+        <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+        <link rel="preconnect" href="https://fonts.gstatic.com"></link>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rammetto+One&display=swap"
+          rel="stylesheet"
+        ></link>
+      </Helmet>
+      <div className="flex justify-center w-1/2 h-64 bg-purple">
+        <div>
+          <div className="text-8xl font-rammetto content-center text-yellow content-center">
+            WATAKUMI
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
