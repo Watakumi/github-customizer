97a1247b91d95952451928394c90825c1180432a
石渡拓海 <t-ishiwata@sight-visit.com>
2021-12-22 02:28:12 +0900

    使用するフォントとカラーパレットの追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   content: ['./src/**/*.{js,jsx,ts,tsx}'],
   theme: {
+    colors: {
+      purple: '#412554',
+      yellow: '#FFE4A8',
+      green: '#008B52',
+    },
+    fontFamily: {
+      rammetto: ['Rammetto One'],
+    },
     extend: {},
   },
   plugins: [],
